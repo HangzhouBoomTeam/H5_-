89068fd2e7436c59ca61fc4b71b2c6b46e3430d1
奕豪杰 <haojie.yi@downtown8.com>
2017-01-15 12:51:02 +0800

    a

--- src/main.js
+++ src/main.js
@@ -1,15 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import VueRouter from 'vue-router'
-import routes from './routers';
+import router from './routers';
 Vue.use(VueRouter);
 
 // 实例化VueRouter
-const router = new VueRouter({
-    mode: 'history',
-    routes
-});
 new Vue({
-    router
+	router
 }).$mount('#app');
-
--- src/routers.js
+++ src/routers.js
@@ -1,38 +1,18 @@
-const App = resolve => {
-    require.ensure(['./App.vue'], () => {
-        resolve(require('./App.vue'));
-    });
-};
-const MyResult =  resolve => {
-    require.ensure(['./MyResult.vue'], () => {
-            resolve(require('./MyResult.vue'));
-        });
-}
-const routers = [
-    {
-    path: '/app',
-    name: 'app',
-    component(resolve) {
-        require.ensure(['./App.vue'], () => {
-            resolve(require('./App.vue'));
-        });
-    }
-},
-{
-    path: '/my',
-    name: 'my',
-    component(resolve) {
-        require.ensure(['./MyResult.vue'], () => {
-            resolve(require('./MyResult.vue'));
-        });
-    }
-},{
-    path:'/',
-    name:'app',
-     component(resolve) {
-        require.ensure(['./App.vue'], () => {
-            resolve(require('./App.vue'));
-        });
-    }
-}];
-export default routers;
+import VueRouter from 'vue-router'
+import MyResult from "./MyResult.vue";
+import App from "./App.vue";
+
+const router = new VueRouter({
+    mode: 'hash',
+    routes: [{
+        name: 'home',
+        path: '/',
+        component: App
+    }, {
+        name: 'my',
+        path: '/my',
+        component: MyResult
+    }]
+})
+
+export default router;

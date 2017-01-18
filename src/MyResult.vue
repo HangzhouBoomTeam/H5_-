6f14c75d52b18f065a90d453100a9121d5e51604
朱衢林 <0xjoker@bombvote.com>
2017-01-18 23:47:25 +0800

    longpress

--- src/MyResult.vue
+++ src/MyResult.vue
@@ -54,7 +54,68 @@ style="position: fixed;top:0;left:0;width: 100%;height:100%;z-index:2200;backgro
 </template>
 <script>
 import http from "./http.js";
+var touchFunc = function(obj,type,func) {
+    //滑动范围在5x5内则做点击处理，s是开始，e是结束
+    var init = {x:5,y:5,sx:0,sy:0,ex:0,ey:0};
+    var sTime = 0, eTime = 0;
+    type = type.toLowerCase();
+  
+    obj.addEventListener("touchstart",function(){
+        sTime = new Date().getTime();
+        init.sx = event.targetTouches[0].pageX;
+        init.sy = event.targetTouches[0].pageY;
+        init.ex = init.sx;
+        init.ey = init.sy;
+        if(type.indexOf("start") != -1) func();
+    }, false);
+  
+    obj.addEventListener("touchmove",function() {
+        event.preventDefault();//阻止触摸时浏览器的缩放、滚动条滚动
+        init.ex = event.targetTouches[0].pageX;
+        init.ey = event.targetTouches[0].pageY;
+        if(type.indexOf("move")!=-1) func();
+    }, false);
+  
+    obj.addEventListener("touchend",function() {
+        var changeX = init.sx - init.ex;
+        var changeY = init.sy - init.ey;
+        if(Math.abs(changeX)>Math.abs(changeY)&&Math.abs(changeY)>init.y) {
+            //左右事件
+            if(changeX > 0) {
+                if(type.indexOf("left")!=-1) func();
+            }else{
+                if(type.indexOf("right")!=-1) func();
+            }
+        }
+        else if(Math.abs(changeY)>Math.abs(changeX)&&Math.abs(changeX)>init.x){
+            //上下事件
+            if(changeY > 0) {
+                if(type.indexOf("top")!=-1) func();
+            }else{
+                if(type.indexOf("down")!=-1) func();
+            }
+        }
+        else if(Math.abs(changeX)<init.x && Math.abs(changeY)<init.y){
+            eTime = new Date().getTime();
+            //点击事件，此处根据时间差细分下
+            if((eTime - sTime) > 300) {
+                if(type.indexOf("long")!=-1) func(); //长按
+            }
+            else {
+                if(type.indexOf("click")!=-1) func(); //当点击处理
+            }
+        }
+        if(type.indexOf("end")!=-1) func();
+    }, false);
+};
 
+var listenLongPress =  function () { 
+    var show_img = document.getElementById('show_img');
+    console.log(show_img);
+        touchFunc(document.getElementById('show_img'),'long',function () {  
+             http.get('guessResult*saveImage');
+         });
+ }
 export default {
   
     data () {
@@ -95,6 +156,7 @@ export default {
                self.src = dataUrl
                // document.getElementById('template').remove()
                document.getElementById('show_img').src = dataUrl
+               listenLongPress();
           })
         },500)
           
@@ -132,8 +194,8 @@ export default {
           this.show_tip =false
        },
        seeMore(){
-        window.location.href=`https://chat.in66.com/pages/peel_hot/list.html?_ig=forecast_jump&location=${this.poss}&location_gps=${this.gps}`
-        http.get('guessResult*lookMore');
+         http.get('guessResult*lookMore');
+          window.location.href=`https://chat.in66.com/pages/peel_hot/list.html?_ig=forecast_jump&location=${this.poss}&location_gps=${this.gps}`
 
        },
        mePlay(){
